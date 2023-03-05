fd669be12f79a0100ea8f458f9ae40c7afcb3c1e
Hussein Hady <hussein.hady@tatware.com>
2023-03-05 18:31:53 +0200

    fix 1 bug sonarqube

--- WebApp.SharedKernel/Consts/FileTypes.cs
+++ WebApp.SharedKernel/Consts/FileTypes.cs
@@ -1,36 +1,37 @@
 ﻿
+using System.Collections.Immutable;
+
 namespace WebApp.SharedKernel.Consts
 {
     public static class FileTypes
     {
         public const string Image = "Image";
-        public static readonly List<string> ImageExtensions = new List<string>() { ".jpg", ".jpeg", ".png" };
+        public static readonly ImmutableList<string> ImageExtensions = ImmutableList.Create(".jpg", ".jpeg", ".png");
 
         public const string Document = "Document";
-        public static readonly List<string> DocumentExtensions = new List<string>() { ".pdf" };
+        public static readonly ImmutableList<string> DocumentExtensions = ImmutableList.Create(".pdf");
 
         public const string ReportDocument = "ReportDocument";
-        public static readonly List<string> ReportDocumentExtensions = new List<string>() { ".pdf", ".xlsx", ".xls", ".docx", ".doc" };
+        public static readonly ImmutableList<string> ReportDocumentExtensions = ImmutableList.Create(".pdf", ".xlsx", ".xls", ".docx", ".doc");
 
         public const string Audio = "Audio";
         public const string Video = "Video";
 
         public static string GetReportExtension(string reportType)
         {
-            var renderType = ReportDocumentExtensions.Where(x => x == ".pdf").Single();
+            var renderType = ReportDocumentExtensions.Single(x => x == ".pdf");
             if (!string.IsNullOrEmpty(reportType))
             {
                 switch (reportType.ToLower())
                 {
                     default:
-                    case "pdf":
-                        renderType = ReportDocumentExtensions.Where(x => x == ".pdf").Single();
+                        renderType = ReportDocumentExtensions.Single(x => x == ".pdf");
                         break;
                     case "word":
-                        renderType = ReportDocumentExtensions.Where(x => x == ".doc").Single();
+                        renderType = ReportDocumentExtensions.Single(x => x == ".doc");
                         break;
                     case "excel":
-                        renderType = ReportDocumentExtensions.Where(x => x == ".xls").Single();
+                        renderType = ReportDocumentExtensions.Single(x => x == ".xls");
                         break;
                 }
             }
