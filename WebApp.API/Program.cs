2e5b38fa694f95ca3ae2d492bd59f57a94e7d4d2
Hussein Hady <hussein.hady@tatware.com>
2023-03-06 14:00:58 +0200

    test sonar

--- WebApp.API/Program.cs
+++ WebApp.API/Program.cs
@@ -149,7 +149,6 @@ builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
