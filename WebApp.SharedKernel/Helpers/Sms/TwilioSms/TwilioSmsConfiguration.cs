33044c247819880e4969e030eb6876a40e9db0e2
Walaa Mahmoud <walaamahmoudosman@gmail.com>
2023-03-06 19:13:38 +0200

    Restore SMS

---
+++ WebApp.SharedKernel/Helpers/Sms/TwilioSms/TwilioSmsConfiguration.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebApp.SharedKernel.Helpers.Sms.TwilioSms
+{
+    public class TwilioSmsConfiguration
+    {
+        public string AccountSID { get; set; }
+        public string AuthToken { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ WebApp.SharedKernel/Helpers/Sms/TwilioSms/TwilioSmsService.cs
@@ -0,0 +1,29 @@
+﻿using WebApp.SharedKernel.Interfaces;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace WebApp.SharedKernel.Helpers.Sms.TwilioSms
+{
+    public class TwilioSmsService : ISmsService
+    {
+        private readonly TwilioSmsConfiguration _twilioSmsConfiguration;
+        public TwilioSmsService(TwilioSmsConfiguration twilioSmsConfiguration)
+        {
+            _twilioSmsConfiguration = twilioSmsConfiguration;
+            TwilioClient.Init(_twilioSmsConfiguration.AccountSID, _twilioSmsConfiguration.AuthToken);
+        }
+        public async Task SendAsync(string mobileNumber, string body)
+        {
+            var result = MessageResource.Create(
+                from: new Twilio.Types.PhoneNumber(_twilioSmsConfiguration.PhoneNumber),
+                body: body,
+                to: mobileNumber);
+        }
+
+        public async Task SendMultiAsync(List<string> lMobileNumbers, string body)
+        {
+            foreach (var mobileNumber in lMobileNumbers)
+                SendAsync(mobileNumber, body);
+        }
+    }
+}
